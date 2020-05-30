22563bc528b3ebfe305776de6f7e89e2e8d68534
Tom Kwong <tk3369@gmail.com>
2020-05-30 07:13:11 +0000

    added install script

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# Install new web site 
+
+path=`pwd`
+
+# generate new site content
+julia --project=. -e 'using Franklin; serve(; clear = true, single = true, cleanup = true)'
+
+# make a backup
+echo "Backing up web site"
+cd /var/www/html
+tar cfz archive/backup-`date +%Y%m%d-%H%M%S`.tgz *.xml *.html *.jl assets css pub layout libs tag
+
+# remove same files
+echo "Removing existing files"
+rm -rf *.xml *.html *.jl assets css pub layout libs tag
+
+# install new files
+echo "Installing new files from $path"
+cd $path
+cp -r __site/* /var/www/html
